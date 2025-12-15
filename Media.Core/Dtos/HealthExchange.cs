d65625badaab393ba61ed3fdf6c51fed9dfd03ee
DustinJoosen <dustinjoosen2003@gmail.com>
2025-12-15 16:30:33 +0100

    Health endpoint now has more information

---
+++ Media.Core/Dtos/HealthExchange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media.Core.Dtos
+{
+    /// <summary>
+    /// Output for a health checkup.
+    /// </summary>
+    /// <param name="Status">"Ok".</param>
+    /// <param name="Environment">Development or Production.</param>
+    /// <param name="Configuration">Debug or Release.</param>
+    /// <param name="Uptime">Running time of the api.</param>
+    /// <param name="StartTime">Starting timestamp of when the api is running.</param>
+    /// <param name="Database">Database information.</param>
+    public record HealthResponse(
+        string Status,
+        string Environment,
+        string Configuration,
+        string Uptime,
+        string StartTime,
+        HealthDatabaseResponse Database);
+
+    /// <summary>
+    /// Output about the database.
+    /// </summary>
+    /// <param name="IsRunning">Is the database running.</param>
+    /// <param name="Server">What server is database on.</param>
+    /// <param name="Version">Version of the database.</param>
+    public record HealthDatabaseResponse(
+        bool IsRunning,
+        string Server,
+        string Version);
+}
--- Media.Presentation/Controllers/HealthController.cs
+++ Media.Presentation/Controllers/HealthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Media.Core.Dtos;
+using Media.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Media.Presentation.Controllers
 {
@@ -9,12 +12,68 @@ namespace Media.Presentation.Controllers
     [ApiController]
     public class HealthController : ControllerBase
     {
+        private static readonly DateTime AppRunningTime = DateTime.UtcNow;
+        private readonly IWebHostEnvironment _env;
+        private readonly MediaDbContext _context;
+
+        public HealthController(IWebHostEnvironment env, MediaDbContext context)
+        {
+            this._env = env;
+            this._context = context;
+        }
+
         /// <summary>
         /// Checks if the API is running fine.
         /// </summary>
         [HttpGet]
         [Route("")]
-        public IActionResult Health() =>
-            this.Ok("Media API is running");
+        public async Task<HealthResponse> Health()
+        {
+            // Environment and configuration.
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+            var configuration = environment == "Development" ? "Debug" : "Release";
+
+            // Uptime of the API.
+            var uptime = (DateTime.UtcNow - AppRunningTime).ToString(@"dd\.hh\:mm\:ss");
+            var startTime = AppRunningTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+            // Database connection.
+            var databaseInformation = await this.GetDatabaseInformation();
+
+            // Output formatting.
+            return new HealthResponse(
+                Status: "Ok",
+                Environment: environment,
+                Configuration: configuration,
+                Uptime: uptime,
+                StartTime: startTime,
+                Database: databaseInformation);
+        }
+
+
+        /// <summary>
+        /// Get the database health information.
+        /// </summary>
+        /// <returns>Database server information.</returns>
+        private async Task<HealthDatabaseResponse> GetDatabaseInformation()
+        {
+            try
+            {
+                await this._context.Database.OpenConnectionAsync();
+                var connection = this._context.Database.GetDbConnection();
+
+                return new HealthDatabaseResponse(
+                    IsRunning: true,
+                    Server: connection.DataSource,
+                    Version: connection.ServerVersion);
+            }
+            catch
+            {
+                return new HealthDatabaseResponse(
+                    IsRunning: false,
+                    Server: "N/A",
+                    Version: "N/A");
+            }
+        }
     }
 }
